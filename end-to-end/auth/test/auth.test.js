deb6bb30e581570a08660f6b3e22163eef3e1e82
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-18 18:37:46 +0300

    src: сервис тестирования end-to-end

---
+++ end-to-end/auth/test/auth.test.js
@@ -0,0 +1,14 @@
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 999999;
+
+beforeAll(async () => {});
+
+afterAll(async () => {});
+
+describe('Auth e2e', () => {
+	test('Signup', async () => {
+		expect(true).toBe(true);
+  }),
+  test('Signin', async () => {
+		expect(true).toBe(true);
+  })
+});
--- gateway-service/src/components/auth/auth.handler.get.js
+++ gateway-service/src/components/auth/auth.handler.get.js
@@ -1,7 +1,8 @@
 const config            = require('../../../config.json').authServiceConfig;
 const rp                = require('request-promise');
 
-module.exports = async (req, res) => {    
+module.exports = async (req, res) => {
+    // TODO добавить обработку параметров
     const uri = `${config.uri}:${config.port}${req.url}`;
     const response = await rp({uri});
     res.send(response);
