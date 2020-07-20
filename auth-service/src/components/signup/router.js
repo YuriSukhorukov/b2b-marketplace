af2b42fa84fed7f8a790b370cbc5a0796d46ba9f
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-20 21:34:05 +0300

    upd

--- auth-service/src/components/signup/router.js
+++ auth-service/src/components/signup/router.js
@@ -5,6 +5,7 @@ router.get(`/`, require('./api/get'));
 router.get(`/email/:email`, require('./api/get.email'));
 router.get(`/login/:login`, require('./api/get.login'));
 router.post(`/user`, require('./api/post.user'));
+
 router.use(`/`, require('./middlewares/all.middle'));
 router.use(`/`, require('./middlewares/all.middle.err'));
 
