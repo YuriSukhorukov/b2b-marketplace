adf6e2c52ea8cb1652227357f5af7a055cb42365
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-20 21:39:08 +0300

    upd

--- auth-service/src/components/signup/router.js
+++ auth-service/src/components/signup/router.js
@@ -2,11 +2,13 @@ const express       = require('express');
 const router        = express.Router();
 
 router.get(`/`, require('./api/get'));
-router.get(`/email/:email`, require('./api/get.email'));
-router.get(`/login/:login`, require('./api/get.login'));
-router.post(`/user`, require('./api/post.user'));
 
 router.use(`/`, require('./middlewares/all.middle'));
 router.use(`/`, require('./middlewares/all.middle.err'));
+
+router.get(`/email/:email`, require('./api/get.email'));
+router.get(`/login/:login`, require('./api/get.login'));
+
+router.post(`/user`, require('./api/post.user'));
 
 module.exports = router;
