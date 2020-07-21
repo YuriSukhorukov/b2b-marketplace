b793b18bc9107c1745e1961afe5e1b1dbfa19ff9
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-22 01:01:51 +0300

    src ref: config перенесен на уровень ниже в src

--- auth-service/index.js
+++ auth-service/index.js
@@ -1,7 +1,7 @@
-const {appConfig}       = require('./config.json');
+const {appConfig}       = require('./src/config.json');
 const app               = require('./src/app');
 
-const port 				= process.env.PORT || appConfig.port;
+const port 				      = process.env.PORT || appConfig.port;
 
 app.listen(port, () => {
   console.log(`Auth server listening on port ${port}`);
