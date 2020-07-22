0e66f95bf79af4a43e75faee6ae07d95608ed620
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-23 01:49:52 +0300

    src: master-service реструктуризация

--- master-service/index.js
+++ master-service/index.js
@@ -1,4 +1,4 @@
-const { runService } = require('./utility/index');
+const { runService } = require('./src/utility/index');
 
 runService('node', [`../gateway-service/index.js`], { shell: true });
 runService('node', [`../auth-service/index.js`,], { shell: true });
