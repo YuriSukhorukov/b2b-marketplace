6891d9fd1835d6ae3a2a34bd7ede243ca21ef846
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-26 01:45:27 +0300

    ref: структура таблицы пользователей

--- auth-service/src/library/db/actions/create.table.users.action.js
+++ auth-service/src/library/db/actions/create.table.users.action.js
@@ -6,11 +6,11 @@ module.exports = async (pool, params) => {
         });
         await client.query(
             `CREATE TABLE users (
-                id SERIAL NOT NULL,
-                name VARCHAR(255) NOT NULL,
-                address TEXT NOT NULL,
-                email VARCHAR(255) NOT NULL,
-                phone VARCHAR(20) NOT NULL
+                user_id SERIAL NOT NULL PRIMARY KEY,
+                username VARCHAR(255) NOT NULL UNIQUE,
+                email VARCHAR(255) NOT NULL UNIQUE,
+                password VARCHAR(255) NOT NULL,
+                created_on TIMESTAMP NOT NULL
             );`, 
             (error, result) => {
                 console.log("client ready:", client.readyForQuery)
