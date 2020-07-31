8b07be23882cf5fcd1af792cdefc06599e8f5223
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-07-31 03:00:06 +0300

    test: заготовка тестов offer-service

--- test.sh
+++ test.sh
@@ -1,7 +1,11 @@
 # Подготовка auth-service
 bash ./auth-service/init.sh
+# Подготовка offer-service
+bash ./offer-service/init.sh
+
 
 # Запуск тестов API 
 cd end-to-end
-npm run test
+npm run test src/test/auth
+npm run test src/test/offer
 cd ..
