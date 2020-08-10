72098d19069e23e0b6529b2aeda24d46bb318389
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-08-11 01:03:52 +0300

    edit: иконки табов

--- ui-web-user/src/containers/Auth/index.js
+++ ui-web-user/src/containers/Auth/index.js
@@ -4,6 +4,7 @@ import Signin from '../../forms/Signin/index';
 import { Tabs } from 'antd';
 import 'antd/dist/antd.css';
 import './style.css';
+import { LoginOutlined, UserAddOutlined } from '@ant-design/icons';
 
 const { TabPane } = Tabs;
 
@@ -13,10 +14,24 @@ export default () => {
     };
     return (
         <Tabs className="auth-form auth-form__tabs_margin_bottom auth-form__tabs_padding" defaultActiveKey="1" onChange={callback} centered={true}>
-            <TabPane tab="Вход" key="1">
+            <TabPane 
+                tab={
+                    <span>
+                        <LoginOutlined />
+                        Вход
+                    </span>
+                } key="1"
+            >
                 <Signin />
             </TabPane>
-            <TabPane tab="Регистрация" key="2">
+            <TabPane
+                tab={
+                    <span>
+                        <UserAddOutlined />
+                        Регистрация
+                    </span>
+                } key="2"
+            >
                 <Signup />
             </TabPane>
         </Tabs>
