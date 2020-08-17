bcbbce0a539ab2dbd7e005d16c8b6151f19a15a0
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-08-17 20:11:42 +0300

    refactor: перенос компонентов

---
+++ ui-web-user/src/components/Card/Offer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Card } from 'antd';
+import 'antd/dist/antd.css';
+
+export default class OfferCard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <Card></Card>
+        );
+    }
+}
---
+++ ui-web-user/src/components/cards.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Offer from './Card/Offer/index';
+
+export default {
+  title: 'Cards'
+};
+
+export const OfferCard = () => <Offer></Offer>;
