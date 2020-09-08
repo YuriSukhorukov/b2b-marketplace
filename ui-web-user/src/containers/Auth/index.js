722772b2c9bcca939dbd84ce06c33dcde25074a9
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-09-08 12:34:57 +0300

    edit: удалены мусорные комментарии

--- ui-web-user/src/containers/Auth/index.js
+++ ui-web-user/src/containers/Auth/index.js
@@ -13,50 +13,17 @@ const { TabPane } = Tabs;
 export default class Auth extends React.Component {
     constructor(props) {
         super(props);
-        console.log(props);
-        console.log(props.location);
         this.props = props;
-
         this.state = { key: 'signin' };
-        // props.history.push('/auth/signin');
     }
     componentWillMount() {
         this.setState(state => {
             return {key: window.location.pathname}
         })
-        // console.log(this.state.key);
-        
-        // history.push('location')
     }
-    // componentDidUpdate(prevProps) {
-    //     if (window.location.pathname != this.state.key)
-    //         this.props.history.push(this.state.key);
-    // //     console.log(prevProps);
-        
-    // //     // will be true
-    // //     const locationChanged =
-    // //       this.props.location !== prevProps.location;
-    // //     console.log(this.props.location);
-        
-    //   }
-      
-    // callback = (key) => {
-    //     console.log('key', key);
-    //     console.log('window.location.pathname', window.location.pathname);
-    //     // console.log(this.props.history);
-        
-    //     // window.location.pathname = key;
-    //     // this.props.history.push('/auth/signin');
-    // };
     render() {
         // const { match, location, history } = this.props;
         // console.log(location);
-        // let history = useHistory();
-        // if (window.location.pathname != this.state.key)
-        //     console.log('redirect');
-            
-            // this.props.history.push(this.state.key);
-
         return(
             // this.props.history.push(key)
             <Tabs style={{width: "500px"}} defaultActiveKey={this.state.key} onChange={this.props.history.push} centered={true}>
