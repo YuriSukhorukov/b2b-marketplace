2c6db3846bd2e12534951330b2484557941b710c
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-09-10 11:13:16 +0300

    edit: mock OfferFeed

--- ui-web-user/src/components/OfferFeed/OfferFeed.js
+++ ui-web-user/src/components/OfferFeed/OfferFeed.js
@@ -2,13 +2,23 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import { observer } from 'mobx-react';
 
+let offers = [1,2,3];
+
 const OfferFeed = observer(class OfferFeed extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
         return(
-            <div>Offer Feed</div>
+            <div>
+                <input placeholder="Искать предложения"></input>
+                <a>Расширенный поиск</a>
+                <div>
+                    {offers.map((value, index) => {
+                        return <li key={index}>{value}</li>
+                    })}
+                </div>
+            </div>
         )
     }
 });
