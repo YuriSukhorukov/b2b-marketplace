e17468c8340edb5208e1fa65f0151158af8a3615
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-09-18 02:18:55 +0300

    edit: menu

--- ui-web-user/src/containers/Menu/index.js
+++ ui-web-user/src/containers/Menu/index.js
@@ -44,13 +44,27 @@ export default class Navigation extends React.Component {
           <Menu.Item key="/offers/saved">Сохраненные<Link to="/offers/saved" /></Menu.Item>
           <Menu.Item key="/offers/proposals">Отклики<Link to="/offers/proposals" /></Menu.Item>
         </SubMenu>
-        <SubMenu key="sub2" title="Мои предложения">
+        <SubMenu key="sub2" title="Найти компании">
+          <Menu.Item key="/company/search">Поиск<Link to="/company/search" /></Menu.Item>
+          <Menu.Item key="/company/saved">Сохраненные<Link to="/company/saved" /></Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub3" title="Мои предложения">
           <Menu.Item key="/offers/published">Опубликованные<Link to="/offers/published" /></Menu.Item>
           <Menu.Item key="/offers/new">Добавить<Link to="/offers/new" /></Menu.Item>
         </SubMenu>
-        
-
-
+        <SubMenu key="sub4" title="Моя компания">
+          <Menu.Item key="/company">Страница компании<Link to="/company" /></Menu.Item>
+          <Menu.Item key="/company/work-history">История работы<Link to="/company/work-history" /></Menu.Item>
+        </SubMenu>
+        <SubMenu key="sub5" title="Настройки">
+          <Menu.Item key="/settings/contact-info">Контактная информация<Link to="/settings/contact-info" /></Menu.Item>
+          <Menu.Item key="/settings/tax-info">Налоговая информация<Link to="/settings/tax-info" /></Menu.Item>
+          <Menu.Item key="/settings/password-and-security">Пароль и безопасность<Link to="/settings/password-and-security" /></Menu.Item>
+        </SubMenu>
+      
+        {/* tetra.ltd/company/idnr/445233024
+        tetra.ltd/company/445233024
+        tetra.ltd/445233024 */}
 
         {/* <SubMenu key="sub1" icon={<HomeOutlined />} title="Предложения">
           <Menu.Item key="1">Поиск</Menu.Item>
