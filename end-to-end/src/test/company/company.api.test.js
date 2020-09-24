b95d5277bbad8b34cc702d5fbe9c8457aba4979c
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-09-24 14:45:19 +0300

    src: company service заготовка

--- end-to-end/src/test/company/company.api.test.js
+++ end-to-end/src/test/company/company.api.test.js
@@ -22,6 +22,21 @@ describe(`Company...`, () => {
         page = await browser.newPage();
         await page.goto(`${config.uri}:${config.port}/api/v1/auth/signin`);
 
+        let result = await page.evaluate(async () => {
+            const response = await fetch(`/api/v1/company/profile`, {
+                method: 'POST',
+                headers: {"content-type": "application/json",},
+                body: JSON.stringify({
+                    type: "ООО",
+                    title: "Иванов и Сидоров",
+                    tax_id: "4447362839",
+                })
+            });
+            // return response.json();
+            return {succes: true};
+        });
+        expect(result.succes).toBe(true);
+
         expect(true).toBe(true);
     });
 });
--- master-service/index.js
+++ master-service/index.js
@@ -3,3 +3,4 @@ const { runService } = require('./src/utility/index');
 runService('node', [`../gateway-service/index.js`], { shell: true });
 runService('node', [`../auth-service/index.js`,], { shell: true });
 runService('node', [`../market-service/index.js`,], { shell: true });
+runService('node', [`../company-service/index.js`,], { shell: true });
