2ebd33181c3d50edcb6e39a98431d7afe514bd8a
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-09-28 00:20:11 +0300

    edit: test upd

--- end-to-end/src/test/market/market.api.test.js
+++ end-to-end/src/test/market/market.api.test.js
@@ -387,13 +387,104 @@ describe(`Market proposals`, () => {
 
 describe(`Market proposals companies details`, () => {
     test('', async () => {
-        
+        page = await browser.newPage();
+        await page.goto(`${config.uri}:${config.port}/api/v1/auth/signin`);
+
+        let result = await page.evaluate(async () => {
+            const signup = async (params) => {
+                return await fetch(`/api/v1/auth/signup`, {
+                    method: 'POST',
+                    headers: params
+                });
+            };
+            const signin = async (params) => {
+                return await fetch(`/api/v1/auth/signin`, {
+                    method: 'POST',
+                    headers: params
+                });
+            };
+            const signout = async () => {
+                return await fetch(`/api/v1/auth/signout`, {
+                    method: 'POST'
+                });
+            };
+            const editCompanyProfile = async (params) => {
+                return await fetch(`/api/v1/company/profile`, {
+                    method: 'POST',
+                    headers: {"content-type": "application/json"},
+                    body: JSON.stringify(params)
+                });
+            };
+            const createOffer = async (params) => {
+                return await fetch(`/api/v1/market/offers`, {
+                    method: 'POST',
+                    headers: {
+                        "content-type": "application/json",
+                    },
+                    body: JSON.stringify(params)
+                });
+            };
+            const getOffers = async () => {            
+                return await fetch(`/api/v1/market/offers`, {
+                    method: 'GET', 
+                    headers: {
+                        "content-type": "application/json"
+                    }
+                });
+            };
+            const createProposal = async (params) => {
+                return await fetch(`/api/v1/market/proposals`, {
+                    method: 'POST',
+                    headers: {"content-type": "application/json"},
+                    body: JSON.stringify(params) // {offerId: offer.id}
+                });
+            };
+            
+            // 1
+            await signup({
+                email: 'alexander@gmail.com',
+                password: 'password12345'
+            });
+            await signin({
+                email: 'alexander@gmail.com',
+                password: 'password12345'
+            });
+            await editCompanyProfile({
+                legal_type: "ООО",
+                company_name: "Иванов и Сидоров",
+                tax_id: "4447362830",
+            });
+            await createOffer({
+                title: "Сгущенка",
+                description: "Оригинальная сгущенка Рогачевъ.",
+                price: 1000000,
+                amount: 249,
+                currency_code: "RUB",
+                offer_type: "SELL",
+                measure_unit_code: "TN",
+                date_expires: new Date().toISOString(),
+                country: "Российская Федерация",
+                city: "Москва"
+            });
+            await signout();
+
+            // 2
+            const response = await signup({
+                email: 'german@gmail.com',
+                password: 'password12345'
+            });
+
+            return response.json();
+        });
+
+        // 1
         // Зарегистрироваться
         // Авторизоваться
         // Создать профиль компании
         // Создать предложение
         // Выйти
 
+        // 2
         // Зарегистрироваться
         // Авторизоваться
         // Создать профиль компании
@@ -401,6 +492,7 @@ describe(`Market proposals companies details`, () => {
         // Создать отклик на предложение
         // Выйти
 
+        // 3
         // Зарегистрироваться
         // Авторизоваться
         // Создать профиль компании
@@ -409,6 +501,16 @@ describe(`Market proposals companies details`, () => {
         // Получить идентификаторы пользователей из откликов
         // Получить информацию о компаниях по идентификаторам пользователей
 
-        expect(true).toBe(true);
+        expect(result.succes).toBe(true);
     })
-})
+});
+
+const sinup = async () => {
+    return await fetch(`/api/v1/auth/signup`, {
+        method: 'POST',
+        headers: {
+            'email': 'yuri@gmail.com',
+            'password': 'sdWE343sx!'
+        }
+    });
+};
